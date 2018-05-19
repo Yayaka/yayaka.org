1cfbc6d486dc910ac702109ed7b3ca4746ade444
Hashiguchi <ryo12redstone@gmail.com>
2018-05-20 05:02:09 +0900

    Apply cc-by-sa-4.0

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -25,6 +25,9 @@ const Layout = ({ children, data }) => (
     >
       {children()}
     </div>
+    <footer>
+      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style={{borderWidth:'0', marginBottom: '0' }} src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+    </footer>
   </div>
 )
 
